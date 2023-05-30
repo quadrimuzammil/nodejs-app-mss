547f68274e2c8a191ce2644e69344e16d16a42de
quadrimuzammil <97599672+quadrimuzammil@users.noreply.github.com>
2023-05-30 12:00:58 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner({
   serverUrl: 'http://34.133.248.234:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
-	    'sonar.projectName': 'Node JS Application - Sample - webhook',
+	    'sonar.projectName': 'Node JS Application - Sample -webhook',
 	    'sonar.projectKey':'nodejs-02',
 	    'sonar.login': 'squ_b588e9092bda73309853b93c13734725fa79e0b9',
 	    //'sonar.login': 'admin',
