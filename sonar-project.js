c24b3f35932747188d49f0b8054bd3b3bbf14852
muzammil <quadrimuzammil@gmail.com>
2023-07-19 23:23:32 +0530

    msg

--- sonar-project.js
+++ sonar-project.js
@@ -1,13 +1,13 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.28.20.207:9000/',
+  serverUrl: 'http://34.66.2.249:9000',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample -webhook',
-	    'sonar.projectKey':'nodejs-02',
-	    'sonar.login': 'squ_9ab4e928d98e48e5e14cd1064dc96c9cf6ce706d',
-	    #'sonar.login': 'admin',
+	    'sonar.projectKey':'nodejsProject',
+	    'sonar.login': 'squ_44ac88cbf5b78ae94b7f2da339e8e6bbcef5be9a',
+	    //'sonar.login': 'admin',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
@@ -17,3 +17,4 @@ sonarqubeScanner({
           //'sonar.inclusions' : 'src/**'
        },
 }, () => {});
+localStorage
